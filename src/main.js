2c6f4ff1190ff7d5b7182f43599689469816c248
Cesare Gallo <cesaregallo0@gmail.com>
2021-09-27 21:23:05 -0600

    Add check square function

--- src/main.js
+++ src/main.js
@@ -1,34 +1,56 @@
 /* Global Variables */
 
-var game = new Game()
+var game = new Game();
 
 /* Query Selectors */
 
 var boardGrid = document. getElementById('boardGrid');
-var playerOneScore = document.getElementById('player1Score');
-var playerTwoScore = document.getElementById('player2Score');
-var resetButton = document.getElementById('resetButton');
-var squares = document.querySelectorAll('.square');
+var playerOneScore = document.getElementById('playerOneScore');
+var playerTwoScore = document.getElementById('playerTwoScore');
 var displayWinner = document.getElementById('displayWinner');
 var nextPlayerTurn = document.getElementById('playersTurn');
+var squares = document.querySelectorAll('.square');
+var resetButton = document.getElementById('resetButton');
 
 /* Event Listeners */
 
 window.addEventListener('load', displayPlayerWins);
+boardGrid.addEventListener('click', function(event) {
+  clickSquare(event);
+});
 
+/* Functions */
 
 function displayPlayerWins() {
-    game.player1.retrieveWinsFromStorage();
-    game.player2.retrieveWinsFromStorage();
-    playerOneScore.innerText = `${game.player1.wins}`;
-    playerTwoScore.innerText = `${game.player2.wins}`;
-  };
+    game.playerOne.retrieveWinsFromStorage();
+    game.playerTwo.retrieveWinsFromStorage();
+    playerOneScore.innerText = `${game.playerOne.wins}`;
+    playerTwoScore.innerText = `${game.playerTwo.wins}`;
+  }
 
-// function retrievedStoredWins() {
-//   playerOneScore.innerText = game.player1.retrieveWinsFromStorage();
-//   playerTwoScore.innerText = game.player2.retrieveWinsFromStorage();
-// }
+  function clickSquare(event) {
+    var gridSquare = event.target;
+    var gridSquareId = event.target.id;
+    if(gridSquare.innerText === '') {
+      gridSquare.innerText = `${game.playerTurn.token}`;
+      console.log(gridSquare);
+      game.updateGameSquare(gridSquareId);
+      game.nextTurn();
+    } else if(gridSquare) {
+      boardGrid.removeEventListener('click', clickSquare);
+    }
+  }
 
+
+function checkGameStatus() {
+  game.checkForWinner();
+  game.checkForDraw();
+  displayResults();
+}
+
+function displayResults() {
+  
+}
 
 function displayHide(display, hide) {
   display.classList.remove('hidden');
