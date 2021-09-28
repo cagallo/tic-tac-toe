cfa2b9049ade43e4e89982db2f774f391900c0fc
Cesare Gallo <cesaregallo0@gmail.com>
2021-09-27 19:08:46 -0600

    Adddisplay wins function to main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
+/* Global Variables */
+
+var game = new Game()
+
 /* Query Selectors */
 
-var game = new Game();
-var boardGrid = document.getElementbyId('boardGrid');
+var boardGrid = document. getElementById('boardGrid');
+var playerOneScore = document.getElementById('player1Score');
+var playerTwoScore = document.getElementById('player2Score');
+var resetButton = document.getElementById('resetButton');
+var squares = document.querySelectorAll('.square');
+var displayWinner = document.getElementById('displayWinner');
+var nextPlayerTurn = document.getElementById('playersTurn');
+
+/* Event Listeners */
+
+window.addEventListener('load', displayPlayerWins);
+
+
+function displayPlayerWins() {
+    game.player1.retrieveWinsFromStorage();
+    game.player2.retrieveWinsFromStorage();
+    playerOneScore.innerText = `${game.player1.wins}`;
+    playerTwoScore.innerText = `${game.player2.wins}`;
+  };
+
+// function retrievedStoredWins() {
+//   playerOneScore.innerText = game.player1.retrieveWinsFromStorage();
+//   playerTwoScore.innerText = game.player2.retrieveWinsFromStorage();
+// }
+
+
+function displayHide(display, hide) {
+  display.classList.remove('hidden');
+  hide.classList.add('hidden');
+}
